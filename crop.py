26865c7f970dff9cc74b4ffbbb56f11391d32094
alonlerner <al663@njit.edu>
2022-11-26 13:13:15 -0600

    cropped images

--- crop.py
+++ crop.py
@@ -1,21 +1,24 @@
 from PIL import Image
- 
-# Opens a image in RGB mode
-im = Image.open(r"./coco_person/000000000036.jpg")
- 
-# Size of the image in pixels (size of original image)
-# (This is not mandatory)
-width, height = im.size
- 
-# Setting the points for cropped image
-left = 168
-top = 151
-right = 449
-bottom = 639
- 
-# Cropped image of above dimension
-# (It will not change original image)
-im1 = im.crop((left, top, right, bottom))
+import json
+
+f = open('person_boxes.json')
+id = 0
+data = json.load(f)
+for box in data['boxes']:
+    # Opens a image in RGB mode
+    im = Image.open(box['image'])
+
+    # Setting the points for cropped image
+    left = box['bbox'][0]
+    top = box['bbox'][1]
+    right = box['bbox'][2]
+    bottom = box['bbox'][3]
+    
+    # Cropped image of above dimension
+    # (It will not change original image)
+    im1 = im.crop((left, top, right, bottom))
+    im1.save(f'./boxes_images/{id}.jpg')
+    id += 1
  
 # Shows the image in image viewer
-im1.show()
+f.close()
